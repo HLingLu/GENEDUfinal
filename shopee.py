da425eb1b6cea158d965e0c72417cce589fbbf92
HLingLu <106131179+HLingLu@users.noreply.github.com>
2022-06-03 12:02:21 +0800

    Update shopee.py
    增加價格和過濾缺貨商品

--- shopee.py
+++ shopee.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import requests
 from bs4 import BeautifulSoup
 import json
 #url編碼
-import urllib.
-
-#aaa
+import urllib
 
 keyword = input("請輸入關鍵字:")
 limit = 50 #回傳數量
@@ -18,9 +22,9 @@ headers = {
 
 #cookies
 s = requests.Session()
-url = 'https://shopee.tw/api/v2/search/product_labels'
+url = 'https://shopee.tw/api/v4/search/product_labels'
 r = s.get(url, headers=headers)
-print(r)
+#print(r)
 
 #搜尋頁網址
 url = f"https://shopee.tw/api/v4/search/search_items/?by=relevancy&keyword={urllib.parse.quote(keyword)}&limit={limit}&newest=0&order=desc&page_type=search&price_max={max_price}&rating_filter={rate}&version=2"
@@ -29,51 +33,51 @@ re = s.get(url, headers=headers)
 
 #確認資料無誤
 if re.status_code == requests.codes.ok:
-    data = json.loads(re.text)
-#print(data)
+    data = re.json()
 
 #總資料數
 totalcount = data['query_rewrite']['ori_total_count']
 #分頁（蝦皮每頁包含五十筆資料 ref:https://ithelp.ithome.com.tw/articles/10232930)
 total_pg = (totalcount // 50) + 1
 data['totalPage'] = total_pg
-#print(totalcount)
-#print(total_pg)
+print(f"total count: {totalcount}")
+print(f"total page: {total_pg}")
 
 #對每個分頁爬蟲
 for num in range(0, total_pg):
-    print("\nPage", num)
+    #print("\nPage", num)
     pg_url = f"https://shopee.tw/api/v4/search/search_items/?by=relevancy&keyword={urllib.parse.quote(keyword)}&limit=50&newest={num*50}&order=desc&page_type=search&version=2"
-    page = num + 1
-    print(pg_url)
+    #page = num + 1
+    
     resp = requests.get(pg_url, headers=headers)
-    doc = json.loads(resp.text)
+    doc = resp.json()
     position = 0
     count = 0
     
-    #擷取分頁內每個商品的商品名稱、網址
-    #print(doc['items'])
+    #擷取分頁內每個商品的資訊
     for d in doc['items']:
-        # print(d)
-        productid = d['itemid']
+        #print(d)
+        productid = d['item_basic']['itemid']
         name = d['item_basic']['name']
-        shopid = d['shopid']
+        shopid = d['item_basic']['shopid']
         item_url = f"https://shopee.tw/product/{shopid}/{productid}" #商品網址
+        #print(f"shopid: {shopid}   productid: {productid}")
         print(name)
         print(item_url)
         
         #商品內頁
-        #re_item_url = f"https://shopee.tw/api/v2/item/get?itemid={productid}&shopid={shopid}" #get的網址
-        #re_item = requests.get(re_item_url, headers=headers)
-        #re_item = requests.get(item_url, headers=headers)
-        #print(re_item)
-        #imf = re_item.json()
-        #soup_item = BeautifulSoup(re_item.text, "html.parser")
-        #print(soup_item.text)
-        #price = soup_item.find("div", {"class":"pmmxKx"}).text
-        #print(price)
-        #selling_price = str(imf['item']['price_max'])[:-5]
-        #print(selling_price)
+        re_item_url = f"https://shopee.tw/api/v4/item/get?itemid={productid}&shopid={shopid}" #get的網址
+        re_item = requests.get(re_item_url, headers=headers)
+        imf = re_item.json()
+        #print(imf)
         
-
+        #每個細項名稱+價格
+        for i, item in enumerate(imf['data']['models']):
+            #過濾已經沒貨的
+            if item['stock'] == 0:
+                continue
+            if len(imf['data']['models']) == 1:
+                print(f"\tprice: {int(item['price']/100000)}\tstock: {item['stock']}")
+            else:
+                print(f"\t{item['name']} price: {int(item['price']/100000)}\tstock: {item['stock']}")
 
